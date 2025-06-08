4dd3e73331634ddcc0711f8587a9db2864a3685e
KiwiSoupfx <Kiwisoupvr@gmail.com>
2025-06-08 00:53:16 -0500

    Update version number

--- SimpleUtilities/SimpleUtilities.cs
+++ SimpleUtilities/SimpleUtilities.cs
@@ -13,7 +13,7 @@ namespace SimpleUtilities
         public override string Name { get; } = "SimpleUtilities";
         public override string Author { get; } = "omgiamhungarian, KiwiSoupfx";
         public override string Description { get; } = "Provides simple features for your server.";
-        public override Version Version { get; } = new Version(1, 4, 1);
+        public override Version Version { get; } = new Version(1, 5, 0);
         public override Version RequiredApiVersion { get; } = new Version(LabApiProperties.CompiledVersion);
         public EventHandlers Events { get; private set; } = new EventHandlers();
         public static SimpleUtilities Singleton { get; set; } = null!;
